04bf67f13e8bb1d4ee0b8b542879f3edd2fda06b
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-10-04 22:16:37 +0600

    iv_laravel multi auth with breeze (a)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AgentController;
+use App\Http\Controllers\ProfileController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +20,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/admin/dashboard', [AdminController::class,'AdminDashboard'])->name('admin.dashboard');
+
+Route::get('/agent/dashboard', [AgentController::class, 'AgentDashboard'])->name('agent.dashboard');
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
